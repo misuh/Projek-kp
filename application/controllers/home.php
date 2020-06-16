cc76df876ecaeb35d1f39e77094c795dd4e9186c
misuh <hafizhjihaad@yahoo.co.id>
2020-06-16 15:45:21 +0700

    Untuk Fitur tinggal Diagram Bar

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,10 +8,21 @@ class Home extends CI_Controller
     	$data['user'] = $this->db->get_where('usr',['email' => 
 		$this->session->userdata('email')]) -> row_array();
     	$data['title'] = 'Home';
+        $sql="SELECT sum(jml) as jml from perfonmasi_jaringan ";
+        $result = $this->db->query($sql);
+        $data['jml'] = $result->row()->jml;
+        $sql="SELECT SEC_TO_TIME( SUM( TIME_TO_SEC( `dur` ) ) ) as dur from perfonmasi_jaringan ";
+        $result1 = $this->db->query($sql);
+        $data['dur'] = $result1->row()->dur;
     	$this->load->view('temp/header',$data);
     	$this->load->view('temp/side',$data);
     	$this->load->view('temp/top',$data);
         $this->load->view('Menu/home',$data);
         $this->load->view('temp/footer',$data);
+       
     }
+
+    // public function sum_(){
+
+    // }
 }
